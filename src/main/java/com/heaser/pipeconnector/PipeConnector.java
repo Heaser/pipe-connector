2d3b7e544f35fcb72b375b42cc63ea91f8407e68
Claycorp <Claycorp@doubledoordev.net>
2023-05-15 01:23:38 -0500

    Fix client event not registering.

--- src/main/java/com/heaser/pipeconnector/PipeConnector.java
+++ src/main/java/com/heaser/pipeconnector/PipeConnector.java
@@ -3,12 +3,12 @@ package com.heaser.pipeconnector;
 import com.heaser.pipeconnector.items.ModItems;
 import com.heaser.pipeconnector.items.pipeconnectoritem.utils.ClientEvents;
 import com.heaser.pipeconnector.network.NetworkHandler;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(PipeConnector.MODID)
 public class PipeConnector {
@@ -23,16 +23,13 @@ public class PipeConnector {
         // This constructor is already registered and ran when the registries exist, we don't need another method to do so.
         NetworkHandler.register();
 
+        // Client things should generally always be separated from the rest.
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            MinecraftForge.EVENT_BUS.register(new ClientEvents());
+        }
+
         // We are using a deferred register, so we can just directly register the registry we make instead of whatever mess
         // there was before with handing off the mod bus all over the place.
         ModItems.ITEMS.register(modEventBus);
-    }
-
-    // Client things should generally always be separated from the rest.
-    // It could also be handled with "if (FMLEnvironment.dist == Dist.CLIENT) {}" in the constructor, but we will just
-    // do this for your sanity's sake.
-    @SubscribeEvent
-    public void clientSetup(FMLClientSetupEvent event) {
-        MinecraftForge.EVENT_BUS.register(new ClientEvents());
     }
 }
