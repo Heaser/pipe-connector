27eefd7246ecd91f3cae85807c8ae3e68e251952
Heaser <heasos@gmail.com>
2023-12-11 21:47:41 +0200

    - Fixed pipe path preview not showing when Embeddium was also installed (HUGE thanks to embeddedt)

--- src/main/java/com/heaser/pipeconnector/client/outline/PipeConnectorRenderType.java
+++ src/main/java/com/heaser/pipeconnector/client/outline/PipeConnectorRenderType.java
@@ -9,15 +9,21 @@ import net.minecraft.client.renderer.RenderType;
 import java.util.OptionalDouble;
 
 public class PipeConnectorRenderType extends RenderType {
-    public static final RenderType LINES_NO_DEPTH_TEST = RenderType.create("pipe_connector_lines", DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.DEBUG_LINES, 256, false, false, RenderType.CompositeState.builder()
-            .setShaderState(new ShaderStateShard(GameRenderer::getPositionColorShader))
-            .setLineState(new LineStateShard(OptionalDouble.empty()))
-            .setLayeringState(RenderStateShard.NO_LAYERING)
-            .setTransparencyState(NO_TRANSPARENCY)
+    public static final RenderType LINES_NO_DEPTH_TEST = RenderType.create("pipe_connector_lines",
+            DefaultVertexFormat.POSITION_COLOR_NORMAL,
+            VertexFormat.Mode.LINES,
+            256,
+            false,
+            false,
+            RenderType.CompositeState.builder()
+            .setShaderState(RENDERTYPE_LINES_SHADER)
+            .setLineState(new RenderStateShard.LineStateShard(OptionalDouble.empty()))
+            .setLayeringState(VIEW_OFFSET_Z_LAYERING)
+            .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+            .setOutputState(ITEM_ENTITY_TARGET)
             .setWriteMaskState(COLOR_DEPTH_WRITE)
-            .setDepthTestState(NO_DEPTH_TEST)
-            .setWriteMaskState(COLOR_WRITE)
             .setCullState(NO_CULL)
+            .setDepthTestState(NO_DEPTH_TEST)
             .createCompositeState(false));
 
     public PipeConnectorRenderType(String name, VertexFormat format, VertexFormat.Mode mode, int bufferSize, boolean affectsCrumbling, boolean sortOnUpload, Runnable setupTask, Runnable clearTask) {
